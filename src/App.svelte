d44554667104e37321ced7d733b3cf95d2f162ec
MeowMerry <meow3525@gmail.com>
2022-08-09 21:14:16 -0500

    create first svelte with TypeScript

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Header from './components/Header.svelte';
+import TodoList from './components/TodoList.svelte'
+interface PersonProps {
+            name: string
+            age: number 
+			description: string
+}
+export const person: PersonProps = {
+            name: 'Todo List',
+            age: 18,
+			description: "Let's get play!",
+}
+</script>
+<main>
+<Header  name={person.name} age={person.age} description={person.description} />
+<TodoList />
+
+</main>
+ 
+<style>
+
+</style>
---
+++ src/components/TodoList.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  interface ListItems {
+    text: string;
+    status: boolean;
+  }
+  let newItem: string = "";
+  let todoList: ListItems[] = [
+    { text: "Write my first post", status: true },
+    { text: "Upload the post to the blog", status: false },
+    { text: "Publish the post at Facebook", status: false },
+  ];
+
+  function addToList() {
+    todoList = [...todoList, { text: newItem, status: false }];
+    newItem = "";
+  }
+
+  function removeFromList(index) {
+    todoList.splice(index, 1);
+    todoList = todoList;
+  }
+</script>
+
+<div class="container-todo">
+  <div>
+    <input bind:value={newItem} type="text" placeholder="new todo item.." />
+    <button on:click={addToList}>Add</button>
+
+    <br />
+    {#each todoList as item, index}
+      <input bind:checked={item.status} type="checkbox" />
+      <span class:checked={item.status}>{item.text}</span>
+      <span on:click={() => removeFromList(index)}>❌</span>
+      <br />
+    {/each}
+    <br />
+  </div>
+</div>
+
+<style>
+  .container-todo {
+    display: flex;
+    justify-content: center;
+  }
+  .checked {
+    text-decoration: line-through;
+  }
+</style>
